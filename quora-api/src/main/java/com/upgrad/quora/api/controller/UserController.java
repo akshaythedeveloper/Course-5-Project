3c41e16acb722cb8f3c724c4166f947a268b8152
askthedeveloper <akshay.verma0808@gmail.com>
2018-10-22 01:27:16 +0530

    Implementing Signup Functionality

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -2,6 +2,9 @@ package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.SignupBusinessService;
+import com.upgrad.quora.service.entity.UsersEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,12 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/")
 public class UserController {
 
+    @Autowired
+    private SignupBusinessService signupBusinessService;
+
     @RequestMapping(method = RequestMethod.POST , path = "/user/signup" , consumes = MediaType.APPLICATION_JSON_UTF8_VALUE , produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) {
-        return new ResponseEntity(HttpStatus.OK);
+
+        final UsersEntity usersEntity = new UsersEntity();
+        usersEntity.setUuid(UUID.randomUUID().toString());
+        usersEntity.setFirstname(signupUserRequest.getFirstName());
+        usersEntity.setLastname(signupUserRequest.getLastName());
+        usersEntity.setEmail(signupUserRequest.getEmailAddress());
+        usersEntity.setPassword(signupUserRequest.getPassword());
+        usersEntity.setSalt("1234abc");
+
+
+        final UsersEntity createdUsersEntity = signupBusinessService.signup(usersEntity);
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUsersEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
+        return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
     }
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/SignupBusinessService.java
@@ -1,11 +1,18 @@
 package com.upgrad.quora.service.business;
 
+import com.upgrad.quora.service.dao.UsersDao;
 import com.upgrad.quora.service.entity.UsersEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignupBusinessService {
+
+    @Autowired
+    private UsersDao usersDao;
+
     public UsersEntity signup(UsersEntity usersEntity) {
+        return usersDao.createUser(usersEntity);
 
     }
 }
